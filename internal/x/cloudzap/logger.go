198ce85a6f9e186e5fcce63c3aeabaec21550bd1
Kilian Demeulemeester <130464061+kiliandbigblue@users.noreply.github.com>
2024-07-18 23:25:53 +0200

    fix/logger-source-location (#9)

--- internal/x/cloudzap/logger.go
+++ internal/x/cloudzap/logger.go
@@ -1,3 +1,4 @@
+//nolint:wrapcheck // Internal stuff.
 package cloudzap
 
 import (
@@ -36,10 +37,56 @@ func NewLogger(config LoggerConfig) (*zap.Logger, error) {
 	zapOptions := []zap.Option{
 		zap.AddCaller(),
 		zap.AddStacktrace(zap.FatalLevel), // add stacktraces manually where needed
+		zap.WrapCore(func(core zapcore.Core) zapcore.Core {
+			return sourceLocationCore{nextCore: core}
+		}),
 	}
 	logger, err := zapConfig.Build(zapOptions...)
 	if err != nil {
 		return nil, fmt.Errorf("init logger: %w", err)
 	}
 	return logger, nil
+}
+
+type sourceLocationCore struct {
+	nextCore zapcore.Core
+}
+
+func (c sourceLocationCore) Enabled(level zapcore.Level) bool {
+	return c.nextCore.Enabled(level)
+}
+
+func (c sourceLocationCore) With(fields []zapcore.Field) zapcore.Core {
+	return sourceLocationCore{
+		nextCore: c.nextCore.With(fields),
+	}
+}
+
+func (c sourceLocationCore) Sync() error {
+	return c.nextCore.Sync()
+}
+
+// Check implements zapcore.Core.
+func (c sourceLocationCore) Check(entry zapcore.Entry, checked *zapcore.CheckedEntry) *zapcore.CheckedEntry {
+	if !c.nextCore.Enabled(entry.Level) {
+		return checked
+	}
+	return checked.AddCore(entry, c)
+}
+
+// Write implements zapcore.Core.
+func (c sourceLocationCore) Write(entry zapcore.Entry, fields []zapcore.Field) error {
+	if entry.Caller.Defined {
+		fields = appendIfNotExists(fields, SourceLocationForCaller(entry.Caller))
+	}
+	return c.nextCore.Write(entry, fields)
+}
+
+func appendIfNotExists(fields []zapcore.Field, field zap.Field) []zapcore.Field {
+	for _, existing := range fields {
+		if existing.Key == field.Key {
+			return fields
+		}
+	}
+	return append(fields, field)
 }
